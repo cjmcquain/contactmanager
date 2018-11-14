a8b24ce23684a138533d2ba726e72aa5fbdf2508
Charles McQuain <charlesmcquain@gmail.com>
2018-11-13 17:43:38 -0800

    Completed JSX Exercise examples

--- src/App.js
+++ src/App.js
@@ -3,9 +3,22 @@ import "./App.css";
 
 class App extends Component {
   render() {
+    const name = "Charles";
+    const showHello = true;
+    const showMath = true;
+
+    let math;
+    if (showMath) {
+      math = <h4>1 + 1 = {1 + 1}</h4>;
+    } else {
+      math = null;
+    }
+
     return (
       <div className="App">
         <h1>The App Component</h1>
+        {showHello ? <h4>Hello {name}</h4> : null}
+        {math}
       </div>
     );
   }
