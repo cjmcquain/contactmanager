6ec3f24b9a3c09d2eecbecb60478474e44e1d695
Charles McQuain <charlesmcquain@gmail.com>
2018-11-26 18:00:26 -0800

    Refactored Contact component

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -2,43 +2,35 @@ import React, { Component } from "react";
 import Contact from "./Contact";
 
 class Contacts extends Component {
-  constructor() {
-    super();
-    this.state = {
-      contacts: [
-        {
-          id: 1,
-          name: "John Doe",
-          email: "jdoe@gmail.com",
-          phone: "555-555-8483"
-        },
-        {
-          id: 2,
-          name: "Bob Jones",
-          email: "bjones@gmail.com",
-          phone: "555-434-8483"
-        },
-        {
-          id: 3,
-          name: "Karen Williams",
-          email: "kwilliams@gmail.com",
-          phone: "222-555-8483"
-        }
-      ]
-    };
-  }
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: "John Doe",
+        email: "jdoe@gmail.com",
+        phone: "555-555-8483"
+      },
+      {
+        id: 2,
+        name: "Bob Jones",
+        email: "bjones@gmail.com",
+        phone: "555-434-8483"
+      },
+      {
+        id: 3,
+        name: "Karen Williams",
+        email: "kwilliams@gmail.com",
+        phone: "222-555-8483"
+      }
+    ]
+  };
   render() {
     const { contacts } = this.state;
 
     return (
       <div>
         {contacts.map(contact => (
-          <Contact
-            key={contact.id}
-            name={contact.name}
-            email={contact.email}
-            phone={contact.phone}
-          />
+          <Contact key={contact.id} contact={contact} />
         ))}
       </div>
     );
