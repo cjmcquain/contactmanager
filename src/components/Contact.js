d902400060b81d9de6a1d3f9bead6b7ae426379f
Charles McQuain <charlesmcquain@gmail.com>
2018-11-26 18:01:09 -0800

    Adjusted PropTypes in Contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -17,9 +17,7 @@ class Contact extends Component {
 }
 
 Contact.propTypes = {
-  name: PropTypes.string.isRequired,
-  email: PropTypes.string.isRequired,
-  phone: PropTypes.string.isRequired
+  contact: PropTypes.object.isRequired
 };
 
 export default Contact;
