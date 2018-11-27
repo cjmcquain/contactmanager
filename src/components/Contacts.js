e0491f8132fc11523f2c45792cd88444f5176e32
Charles McQuain <charlesmcquain@gmail.com>
2018-11-26 17:57:12 -0800

    Created Contacts component with contacts stored in state

---
+++ src/components/Contacts.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import Contact from "./Contact";
+
+class Contacts extends Component {
+  constructor() {
+    super();
+    this.state = {
+      contacts: [
+        {
+          id: 1,
+          name: "John Doe",
+          email: "jdoe@gmail.com",
+          phone: "555-555-8483"
+        },
+        {
+          id: 2,
+          name: "Bob Jones",
+          email: "bjones@gmail.com",
+          phone: "555-434-8483"
+        },
+        {
+          id: 3,
+          name: "Karen Williams",
+          email: "kwilliams@gmail.com",
+          phone: "222-555-8483"
+        }
+      ]
+    };
+  }
+  render() {
+    const { contacts } = this.state;
+
+    return (
+      <div>
+        {contacts.map(contact => (
+          <Contact
+            key={contact.id}
+            name={contact.name}
+            email={contact.email}
+            phone={contact.phone}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Contacts;
